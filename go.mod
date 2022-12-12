ae710e600e6ee9ce7c121258539439df1010f206
crgimenes <crg@crg.eti.br>
2022-12-12 01:47:19 -0300

    update log

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.19
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0
-	github.com/nuveo/log v0.0.0-20190430190217-44d02db6bdf8
 	gopkg.in/ini.v1 v1.67.0
 )
 
--- helper/helper.go
+++ helper/helper.go
@@ -3,13 +3,13 @@ package helper
 import (
 	"io"
 
-	"github.com/nuveo/log"
+	"log"
 )
 
 // Closer close descriptor to use with defer.
 func Closer(f io.Closer) {
 	err := f.Close()
 	if err != nil {
-		log.Errorln(err)
+		log.Println(err)
 	}
 }
